2bc17e7c5a9ea9fa8ec447cb329c9ff77dfeb1ec
pedro8811 <pedrouniessa881@gmail.com>
2022-08-11 16:32:16 -0300

    11 08 2022

--- rascunho/index.js
+++ rascunho/index.js
@@ -1,4 +1,5 @@
-function mostra(num){
+// ---------CALLBACK---------
+/*function mostra(num){
     console.log(num)
 }
 
@@ -14,4 +15,19 @@ function multiplicacao(a, b, cb){
 
 soma(2, 2, mostra);
 
-multiplicacao(2, 4, mostra)
+multiplicacao(2, 4, mostra)*/
+//---------------------------
+
+
+
+function chunkArrayInGroups(arr, size) {
+    let newArray = []
+    for(let i = 0; i< arr.length; i++){
+        for(let i = 1; i < size; i++){
+            
+        }
+    }
+    return arr.slice(1,3);
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
