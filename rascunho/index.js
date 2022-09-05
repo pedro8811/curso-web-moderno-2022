27775b227a077c74052fd6d224c7dfd4a7561aca
pedro8811 <pedrouniessa881@gmail.com>
2022-09-05 13:44:56 -0300

    teste

--- rascunho/index.js
+++ rascunho/index.js
@@ -1,13 +1,16 @@
-function destroyer(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let z = 1; z < arguments.length; z++) {
-      if (arr[i] == arguments[z]) {
-        arr.splice(i, 1);
-      }
-    }
-    arr.splice(i, 1);
-  }
+function whatIsInAName(collection, source) {
+  const arr = [];
+
   return arr;
 }
 
-console.log(destroyer([2, 3, 2, 3], 2, 3));
+console.log(
+  whatIsInAName(
+    [
+      { first: "Romeo", last: "Montague" },
+      { first: "Mercutio", last: null },
+      { first: "Tybalt", last: "Capulet" },
+    ],
+    { last: "Capulet" }
+  )
+);
