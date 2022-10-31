49b1192b824ba37b7e2bbab8e3a7d3b15032a3df
pedro8811 <pedrouniessa881@gmail.com>
2022-10-31 16:48:41 -0300

    segundou

--- ajax/server.js
+++ ajax/server.js
@@ -29,6 +29,21 @@ app.post('/upload', (req, res) => {
     res.end('Concluído com sucesso.')
   })
 })
+app.post('/formulario', (req, res) => {
+  res.send({
+    ...req.body,
+    id: 2
+  })
+})
 
-app.get('/teste', (req, res) => res.send(new Date))
+app.get('/parOuImpar', (req, res) => {
+  // req.body
+  // req.query
+  // req.params
+  const par = parseInt(req.query.numero) % 2 === 0
+  res.send({
+    resultado: par ? 'par' : 'impar'
+  })
+})
+
 app.listen(8080, () => console.log('Executando...'))
